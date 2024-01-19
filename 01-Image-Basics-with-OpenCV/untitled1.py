e5d9757e7021c3a8011f4c6c619a4feb9823dc46
wolworf <serdar.jr.python@gmail.com>
2024-01-19 12:54:34 +0000

    Initial commit with all coursework modules

---
+++ 01-Image-Basics-with-OpenCV/untitled1.py
@@ -0,0 +1,21 @@
+import cv2
+import numpy as np
+
+def create_circle(event,x,y,flags,param):
+    if event == cv2.EVENT_RBUTTONDOWN:
+        cv2.circle(img,(x,y),100,(0,0,255),thickness=10)
+        
+img = cv2.imread("DATA/dog_backpack.png")
+
+cv2.namedWindow(winname='dog')
+
+cv2.setMouseCallback('dog',create_circle)
+
+while True:
+    cv2.imshow('dog',img)
+    if cv2.waitKey(20) & 0xFF == 27:
+        break
+cv2.destroyAllWindows()
+
+
+
